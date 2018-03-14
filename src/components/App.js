d7d330673c945eb9002870d7438372bd7de2fa58
rocciescobar <roccisen.88@gmail.com>
2018-03-14 09:47:58 -0500

    Add props

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,13 @@ import Header from './Header';
 import List from './List';
 import Section from './Section';
 
-const data = [
-  {bio: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim quae similique, voluptatum molestias voluptatem inventore magni atque obcaecati rerum quo cupiditate quis libero recusandae sunt tempore quasi consequatur non perspiciatis.', url: 'yo.jpg'}
-];
+const data = [{bio: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim quae similique, voluptatum molestias voluptatem inventore magni atque obcaecati rerum quo cupiditate quis libero recusandae sunt tempore quasi consequatur non perspiciatis.', url: 'yo.jpg'}];
 const hobbies = ['Bailar', 'Escuchar música', 'Ver series'];
 
 const App = () => (
   <div>
     <Header />
-    <Section data={data} />
+    <Section data={data[0]} />
     <List hobbies={hobbies}/>
   </div>
 )
